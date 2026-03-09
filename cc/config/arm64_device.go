5a4d4cffe47129762e3fc5ca0a5607b97214bab5
BladeRunner-A2C <creator4983@aospa.co>
2026-03-09 19:40:56 +0100

    Disable scalable vector extension for armv9 architectures
    Qualcomm devices do not support SVE.
    As of right now, we do not support non-Qualcomm devices
    with SVE, such as Tensor and Mediatek.
    If that changes, we can revisit this commit.
    But until then, we will use the existing arch variants
    to keep things simple.
    
    Change-Id: If92be31b62a08b4b8599babb854eccaf1629b822
    Suggested-by: Jake Weinstein <jake@aospa.co>

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -34,10 +34,10 @@ var (
 		"armv8-2a-dotprod":   {"-march=armv8.2-a+dotprod"},
 		"armv8-5a":           {"-march=armv8.5-a"},
 		"armv8-7a":           {"-march=armv8.7-a"},
-		"armv9-a":            {"-march=armv9-a+crypto"},
-		"armv9-2a":           {"-march=armv9.2-a+crypto"},
-		"armv9-3a":           {"-march=armv9.3-a+crypto"},
-		"armv9-4a":           {"-march=armv9.4-a+crypto"},
+		"armv9-a":            {"-march=armv9-a+crypto+nosve"},
+		"armv9-2a":           {"-march=armv9.2-a+crypto+nosve"},
+		"armv9-3a":           {"-march=armv9.3-a+crypto+nosve"},
+		"armv9-4a":           {"-march=armv9.4-a+crypto+nosve"},
 	}
 
 	arm64ArchFeatureCflags = map[string][]string{
